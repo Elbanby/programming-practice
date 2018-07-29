cd71784ad7211eb0e70af619f8894affe32a75db
elbanby <elbanby@elbanbys-MacBook-Pro.local>
2018-07-29 16:13:11 -0400

    or35 problem

---
+++ Or35.java
@@ -0,0 +1,10 @@
+/*
+Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator
+http://codingbat.com/prob/p112564
+/*
+
+public boolean or35(int n) {
+  return (n % 3 == 0 || n% 5 == 0)? true : false;
+}
+
+
